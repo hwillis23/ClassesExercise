9f155bb13ac2ce136ae94457c07e18f6a7656a21
Hannah Willis <hwillis8817@gmail.com>
2022-03-30 11:02:33 -0500

    init complete

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            var myCar = new Car(); //new instance of the car class with a new object 
+
+            myCar.Year = 2022;
+            myCar.Model = "Stingray";
+            myCar.Make = "Corevette";
+            
+            
+
+           Console.WriteLine($"I just bought a black {myCar.Year} {myCar.Model} {myCar.Make}.");
+
+
+
+           
+
+
+        }
+    }
+}
